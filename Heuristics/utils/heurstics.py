b802318dc0f74c2c758bd02e7bd8523136d16556
Marouane <MarouaneBenbetka@gmail.com>
2024-03-12 22:55:42 +0100

    feat: integrating palmer heuristic

--- Heuristics/utils/heurstics.py
+++ Heuristics/utils/heurstics.py
@@ -98,3 +98,70 @@ def ham_heuristic(processing_times: np.ndarray) -> list:
         return solution1, Cmax1
     else:
         return solution2, Cmax2
+
+
+# ---------------------------------#
+
+# PALMER
+
+
+def _init_weights(nb_machines):
+    lst = np.array([(2*i - 1 - nb_machines) for i in range(nb_machines)])
+    return lst - np.mean(lst)
+
+
+def _compute_weighted_sum(dist_mat, weights, nb_jobs, nb_machines):
+    # Adjusted to work with dist_mat having dimensions [nb_jobs, nb_machines]
+    weighted_sum = []
+    for i in range(nb_jobs):
+        somme = np.dot(dist_mat[i, :], weights)
+        weighted_sum.append(somme)
+    return np.array(weighted_sum)
+
+
+def _update_c(dist_mat, c, ordre, nb_machines, nb_jobs):
+    # Adjusted for dist_mat with dimensions [nb_jobs, nb_machines]
+    c[0][0] = dist_mat[ordre[0], 0]
+    for j in range(1, nb_machines):
+        c[0][j] = dist_mat[ordre[0], j] + c[0][j-1]
+    for i in range(1, nb_jobs):
+        c[i][0] = dist_mat[ordre[i], 0] + c[i-1][0]
+    for i in range(1, nb_jobs):
+        for j in range(1, nb_machines):
+            c[i][j] = max(c[i-1][j], c[i][j-1]) + dist_mat[ordre[i], j]
+    return c
+
+
+def _get_cmax(c, ordre_opt, nb_machines):
+    return c[len(ordre_opt)-1][nb_machines-1]
+
+
+def _lower_bound(dist_mat, nb_machines, nb_jobs):
+    # Adjusted for dist_mat with dimensions [nb_jobs, nb_machines]
+    lb = []
+    for i in range(nb_machines):
+        a = dist_mat.sum(axis=0)
+        if i == 0:
+            bound = a[i] + min(dist_mat[:, 1:].sum(axis=0))
+        elif i == (nb_machines - 1):
+            bound = a[i] + min(dist_mat[:, :i].sum(axis=0))
+        else:
+            bound = a[i] + min(dist_mat[:, :i].sum(axis=0)
+                               ) + min(dist_mat[:, i+1:])
+        lb.append(bound)
+    return max(lb)
+
+
+def run_palmer(dist_mat):
+    nb_jobs, nb_machines = dist_mat.shape
+    weights = _init_weights(nb_machines)
+
+    weighted_sum = _compute_weighted_sum(
+        dist_mat, weights, nb_jobs, nb_machines)
+    ordre_opt = np.argsort(weighted_sum)[::-1]
+
+    makespan = calculate_makespan(dist_mat, ordre_opt)
+
+    return ordre_opt, makespan
+
+# Remember to provide dist_mat as a NumPy array with dimensions [nb_jobs, nb_machines] to run this function.
